a31e300c89350208476816b2f3149e6e540d4dec
Guruprasath Gopal <guruprasath3200@gmail.com>
2024-05-03 09:44:36 +0800

    Fixed app declaration issue

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-app.use(cors())
 const app = express()
+app.use(cors())
 app.use(express.json())
 morgan.token('requestData', (req) => {
     return JSON.stringify(req.body);
